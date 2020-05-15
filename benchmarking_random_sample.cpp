23afb2bea4cab1e14bf9e5ddfcdd1dc7aeeaf4f9
atarng@github.com <atarng@github.com>
2020-05-14 22:03:09 -0400

    Cleaned up code and removed some extra stuff

--- benchmarking_random_sample.cpp
+++ benchmarking_random_sample.cpp
@@ -11,6 +11,7 @@ using namespace std;
 std::vector<int> g_population;
 std::mt19937 g_gen;
 constexpr int TRIALS = 1000;
+constexpr int CHECK_DISTRIBUTION_LIMIT = 50;
 
 int ModifiedBinarySearch(const vector<int>& collection, int raw_rand) {
   int offset = 0;
@@ -47,14 +48,13 @@ vector<int> GenerateRandomSample(int range, int samples) {
   for(int i = 0; i < samples; ++i) {
     auto raw_rand = rand() % (range - to_exclude.size());
 
-    // binary attempt.    
+    // binary search rather than linear search
     int offset = ModifiedBinarySearch(to_exclude, raw_rand);
+    
     // This part can be optimized as a binary search
     // int offset = 0;
     // while(offset < to_exclude.size() &&
-    //     (raw_rand+offset) >= to_exclude[offset]) {
-    //   offset++;
-    // }
+    //  (raw_rand+offset) >= to_exclude[offset]) offset++;
     
     int to_insert = (raw_rand + offset);
     to_exclude.insert(to_exclude.begin() + offset, to_insert);
@@ -72,16 +72,20 @@ vector<int> GenerateRandomSample(int range, int samples) {
   return solution;
 }
 
-// preallocating gen and using the same one over multiple times is faster.
-// std::vector<int> otherRandomSample(int N, int k, std::mt19937& gen) {
-std::vector<int> robertFloydRandomSample(int N, int k) {
-  std::vector<int> solution; // (N, 0): preallocating and filling by index is slower.
+// This is a deterministic way of generating the set.
+// Accepted answer to: https://tinyurl.com/yadmswnw
+std::unordered_set<int> robertFloydRandomSampleSet(int N, int k) {
   std::unordered_set<int> elems;
 
+  // Init if we care about insertion order.
+  // (K, 0): preallocating and filling by index is slower.
+  // std::vector<int> solution;
+
   for (int r = N - k; r < N; ++r) {
-    // No significant improvement using rand.
+    // No significant improvement using rand
     // auto v = rand() % r;
     int v = std::uniform_int_distribution<>(0, r)(g_gen);
+    
 
     // there are two cases.
     // v is not in candidates ==> add it
@@ -91,12 +95,13 @@ std::vector<int> robertFloydRandomSample(int N, int k) {
     if (!elems.insert(v).second) {
       // printf("duplicate(%d), insert max(%d)\n", v, r);
       elems.insert(r);
-      
-      // Turns out this is actually slower: solution[r-k] = r;
-      solution.push_back(r);
-    } else {
-      // Turns out this is actually slower: solution[r-k] = v;
-      solution.push_back(v);
+
+    // Init if we care about insertion order.
+    //   // Turns out this is actually slower: solution[r-k] = r;
+    //   solution.push_back(r);
+    // } else {
+    //   // Turns out this is actually slower: solution[r-k] = v;
+    //   solution.push_back(v);
     }
   }
   
@@ -104,11 +109,27 @@ std::vector<int> robertFloydRandomSample(int N, int k) {
   // for (auto s : solution) cout << s << " ";
   // cout << endl;
 
-  std::shuffle(solution.begin(), solution.end(), g_gen);
+  // If we don't actually care about knowing what order the elements
+  // are generated in then we can just copy it into the vector here.
+  // std::vector<int> solution(elems.begin(), elems.end());
+  // std::shuffle(solution.begin(), solution.end(), g_gen);
+  // return solution;
 
+  return elems;
+}
+
+// preallocating gen and using the same one over initializing it each trial
+// is significantly faster. Switched to using global instead of pass by ref.
+// std::vector<int> otherRandomSample(int N, int k, std::mt19937& gen) {
+std::vector<int> robertFloydRandomSample(int N, int k) {
+  // This part could be making it too slow (copy constructor)...
+  auto as_set = robertFloydRandomSampleSet(N, k);
+  std::vector<int> solution(as_set.begin(), as_set.end());
+  std::shuffle(solution.begin(), solution.end(), g_gen);
   return solution;
 }
 
+// Prepopulate so we don't do this per trial.
 void PrepopulateRange(int N, std::vector<int>& population) {
   population = vector(N, 0);
   for(int i = 0; i < population.size(); ++i) {
@@ -117,7 +138,7 @@ void PrepopulateRange(int N, std::vector<int>& population) {
 }
 
 // std::vector<int> SampleFromPopulatedRange(int K, const std::vector<int>& population) {
-std::vector<int> SamplePopulatedRange(int dummy_N, int K) {
+std::vector<int> SamplePopulatedRange(int /*dummy_N*/, int K) {
   std::vector<int> samples;
   std::sample(g_population.begin(), g_population.end(),
               std::back_inserter(samples),
@@ -127,10 +148,9 @@ std::vector<int> SamplePopulatedRange(int dummy_N, int K) {
 
 void BenchMarkingHelper(int N, int K,
                         std::function<vector<int>(int, int)> to_execute) {
-
   int count = 0;
   vector<int> nums(N, 0);
-  if(N <= 50) {
+  if(N <= CHECK_DISTRIBUTION_LIMIT) {
     while(count++ < TRIALS) {
       auto sample =
           to_execute(N, K);
@@ -144,7 +164,7 @@ void BenchMarkingHelper(int N, int K,
     }
   }
 
-  if(N <= 50) {
+  if(N <= CHECK_DISTRIBUTION_LIMIT) {
     cout << "distribution:" << endl;
     for(int i = 0; i < nums.size(); ++i) {
       printf("[%s%d]: %s%d%c",
@@ -156,10 +176,20 @@ void BenchMarkingHelper(int N, int K,
   }
 }
 
+// To allow us to use the Robert Floyd method without doing a
+// copy constructor into the vector.
+void BenchMarkingHelperSet(int N, int K,
+    std::function<unordered_set<int>(int, int)> to_execute) {
+  int count = 0;
+  while(count++ < TRIALS) {
+    to_execute(N, K);
+  }
+}
+
 int main() {
   srand(time(NULL));
   constexpr int N = 500;
-  constexpr int K = 200;
+  constexpr int K = 100;
 
   auto end = std::chrono::system_clock::now();
   auto start = std::chrono::system_clock::now();
@@ -175,12 +205,18 @@ int main() {
   std::random_device rd;
   // changed to a global variable to not impact benchmarking.
   g_gen = std::mt19937(rd());
-  
-  start = std::chrono::system_clock::now();
+ 
+   start = std::chrono::system_clock::now();
   BenchMarkingHelper(N, K, robertFloydRandomSample);
   end = std::chrono::system_clock::now();
   diff = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
-  printf("execution of robertFloyd: %lums\n\n", diff.count());
+  printf("execution of robertFloyd: %lums\n", diff.count()); 
+ 
+  start = std::chrono::system_clock::now();
+  BenchMarkingHelperSet(N, K, robertFloydRandomSampleSet);
+  end = std::chrono::system_clock::now();
+  diff = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
+  printf("execution of robertFloydSet: %lums\n\n", diff.count());
 
   // Prepopulate range collection to not impact the benchmarking
   // std::vector<int> population; changed to a global variable to fit
@@ -190,7 +226,7 @@ int main() {
   BenchMarkingHelper(N, K, SamplePopulatedRange);
   end = std::chrono::system_clock::now();
   diff = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
-  printf("execution of SamplePopulatedRange: %lums\n\n", diff.count());
+  printf("execution of Standard: %lums\n\n", diff.count());
 
-  return 0;
+	return 0;
 }
